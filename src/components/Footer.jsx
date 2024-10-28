36c80e474d70069f6a66eb81069776110678e833
Gaurav Bansal <gbgauravbansal007@gmail.com>
2024-10-28 18:31:58 +0530

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import facebook from "/assets/facebook.png";
+import github from "/assets/github-mark.png";
 import instagram from "/assets/instagram.png";
 import linkedin from "/assets/linkedin.png";
 import logo from "/assets/logo.png";
@@ -9,9 +9,13 @@ export default function Footer() {
 
   return (
     <div className="mt-20">
+
+      {/* Main footer section with gradient background and padding */}
       <footer className="bg-gradient-to-b from-blue-100 to-blue-50 text-white pb-10 pt-10 pl-5 pr-5  ">
         <div className="container mx-auto flex flex-col md:flex-row justify-between">
 
+
+          {/* Logo and tagline section */}
           <section className="mb-4 md:mb-0">
             <img
               onClick={() => navigate("/")}
@@ -22,6 +26,7 @@ export default function Footer() {
             <p className="text-lg text-black">Discover Events That Inspire – Explore, Engage, Enjoy!</p>
           </section>
 
+          {/* Links to policies section */}
           <section className="mb-4 md:mb-0 text-black">
             <nav>
               <ul className="list-none space-y-2">
@@ -38,20 +43,28 @@ export default function Footer() {
             </nav>
           </section>
 
+          {/* Contact information section */}
           <section className="mb-4 md:mb-0 text-black">
             <h4 className="font-semibold">Contact Us</h4>
             <ul className="list-none mt-2">
-              <li>eventspotlite@gmail.com</li>
-              <li>7696881805</li>
+              <li>gbgauravbansal007@gmail.com</li>
             </ul>
           </section>
 
+          {/* Social links section */}
           <section className="text-black">
             <h4 className="font-semibold">Social Links</h4>
             <div className="flex space-x-2 mt-2">
-                <img src={instagram} alt="Instagram" className="cursor-pointer h-11 w-auto hover:opacity-80" />
-                <img src={facebook} alt="Facebook" className="cursor-pointer h-11 w-auto hover:opacity-80" />
+
+              {/* Link to GitHub profile */}
+              <a href="https://github.com/gbgaurav007" target="_blank" rel="noopener noreferrer">
+                <img src={github} alt="Github" className="cursor-pointer h-11 w-auto hover:opacity-80" />
+              </a>
+
+              {/* Link to LinkedIn profile */}
+              <a href="https://linkedin.com/in/gauravbansal007" target="_blank" rel="noopener noreferrer">
                 <img src={linkedin} alt="LinkedIn" className="cursor-pointer h-11 w-auto hover:opacity-80" />
+              </a>
             </div>
           </section>
         </div>
